181b6efbd7e62e429a89347ba8f7c10ff265f0b7
Daniel Hunter <dan.hunter55@yahoo.com>
2023-07-07 15:44:16 +0100

    initail commit

---
+++ MarsRoverKata/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Program execution starts here
+        Console.WriteLine("Hello, World!");
+
+        // Wait for user input before exiting
+        Console.ReadLine();
+    }
+}
