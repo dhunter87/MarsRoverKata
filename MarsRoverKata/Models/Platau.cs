50d31dc3ace2e37bfa2135591de431fcb7c28b92
Daniel Hunter <dan.hunter55@yahoo.com>
2023-07-08 19:31:50 +0100

    add initial Platau unit test

---
+++ MarsRoverKata/Models/Platau.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MarsRoverKata.Models
+{
+    public class Platau
+    {
+        public int MaxYCoordinate;
+        public int MaxXCoordinate;
+
+        public Platau(int maxXCoordinate, int maxYCoordinate)
+        {
+            
+        }
+    }
+}
+
---
+++ MarsRoverShould/PlatauUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MarsRoverKata.Models;
+using NUnit.Framework;
+
+namespace PlatauShould
+{
+	[TestFixture]
+	public class PlatauUnitTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+
+		}
+
+		[TestCase()]
+		public void Platau_Should_Not_Be_Null_When_Initialised_With_Coordinates()
+		{
+			var platau = new Platau(5, 5);
+
+			Assert.That(platau.MaxXCoordinate, Is.EqualTo(5));
+			Assert.That(platau.MaxYCoordinate, Is.EqualTo(5));
+
+        }
+	}
+}
+
