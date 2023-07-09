6060261aa675eaaf3aefb4a2ef57507612e7a7f9
Daniel Hunter <dan.hunter55@yahoo.com>
2023-07-09 08:54:14 +0100

    add initial game "MarsMission" test

---
+++ MarsRoverShould/MarsMissionUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MarsRoverKataUnitTests
+{
+	[TestFixture]
+	public class MarsMissionUnitTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+
+		}
+
+		[Test]
+		public void MarsMissionShould_Not_Be_Null_When_Initialised()
+		{
+			var masrMission = new MarsMission();
+
+			Assert.That(masrMission, Is.Not.Null);
+		}
+	}
+
+    internal class MarsMission
+    {
+        public MarsMission()
+        {
+			throw new NotImplementedException();
+        }
+    }
+}
+
